ba92682dcfb804032d62a0158e749e507b2ea51b
Pineapple217 <rosierjules@gmail.com>
2023-10-22 15:16:34 +0200

    tailwind build fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.html", "./static/private/**/*.html", "*.templ"],
+  content: [
+    "./views/**/*.html",
+    "./static/private/**/*.html",
+    "./components/**/*.templ",
+  ],
   theme: {
     extend: {},
   },
